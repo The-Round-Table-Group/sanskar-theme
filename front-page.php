4e9f77fcaed9d1113a7dcc5fbd518b5dcfd92fd2
Shane Schroll <schroll24@gmail.com>
2024-03-24 14:28:55 -0500

    more post setup / setup home featured post

--- front-page.php
+++ front-page.php
@@ -4,5 +4,13 @@ $context = Timber::get_context();
 $post = Timber::get_post();
 $context['post'] = $post;
 
+$context['featured_news'] = Timber::get_terms([
+    'taxonomy'   => 'news-tax',
+    'name'       => 'featured',
+    'orderby'    => 'name',
+    'order'      => 'ASC',
+    'hide_empty' => false
+]);
+
 $templates = ['pages/home.twig'];
 Timber::render( $templates, $context );
