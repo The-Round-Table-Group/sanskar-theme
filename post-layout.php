bbab8d2fa0fda0f35866a3122f4512b1b3799941
Shane Schroll <schroll24@gmail.com>
2024-04-02 11:33:28 -0500

    added press release to footer / renamed about template to post layout for reuse

--- post-layout.php
+++ post-layout.php
@@ -1,7 +1,7 @@
 <?php
-// Template Name: About Us Template
+// Template Name: Post Layout Template
 $context = Timber::get_context();
 $post = Timber::get_post();
 $context['post'] = $post;
-$templates = ['pages/about.twig'];
+$templates = ['pages/post-layout.twig'];
 Timber::render( $templates, $context );
